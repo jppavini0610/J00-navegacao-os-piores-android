8382fc8f22cdeec513feb3386fc0acd561cd457a
Joao Pedro Pavini Cardoso <jppavinigit@hotmail.com>
2021-03-26 22:48:40 -0300

    Equalização Final projeto

--- app/src/main/java/com/example/jogodavelha/MainActivity.java
+++ app/src/main/java/com/example/jogodavelha/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, JogodaVelhaActivity.class));
             }
         });
-
+//testes commit
         botaoCriarPartida.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
